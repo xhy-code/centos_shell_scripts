ee16f97b7fdfe04e7350336527fc52772a03ae36
xhy-code <78998880+xhy-code@users.noreply.github.com>
2021-09-25 14:57:42 +0100

    Update user_config.sh
    the first shellscrpit need to be runing after system reinstall

--- user_config.sh
+++ user_config.sh
@@ -1,4 +1,27 @@
 #!bin/sh
 
+echo "[step1]:Add root user:xhy..."
 text='xhy:x:0:0:root:/root:/bin/bash'
 sed -i "1a ${text}" /etc/passwd
+passwd xhy
+
+echo "[step2]:Add ssh user:xhyssh..."
+useradd -d /home/xhyssh -m xhyssh -s /bin/sh
+passwd xhyssh
+chmod u+w /etc/sudoers
+text='xhyssh  ALL=(ALL) ALL'
+sed -i "100a ${text}" /etc/sudoers
+
+echo "[step3]:configure ssh"
+text='Port 65447'
+sed -i "17a ${text}" /etc/ssh/sshd_config
+text='PermitRootLogin no'
+sed -i "38c ${text}" /etc/ssh/sshd_config
+text='#user_config'
+sed -i "140a ${text}" /etc/ssh/sshd_config
+text='AllowUsers xhyssh'
+sed -i "$a ${text}" /etc/ssh/sshd_config
+text='Protocol 2'
+sed -i "$a ${text}" /etc/ssh/sshd_config
+
+service sshd restart
