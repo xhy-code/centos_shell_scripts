1f318d827c6e16ac7bf3fda306db0973665fae48
xhy-code <78998880+xhy-code@users.noreply.github.com>
2021-10-17 15:06:39 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,29 +18,20 @@ yum install make gcc gcc-c++
 echo "[step5]:安装perl,python,python3......"
 yum install perl python python3
 
-echo "[step6]:创建新用户........"
-echo "是否创建新用户：(y/n)"
-read y_n
-until [ $y_n ]
-do 
-	read y_n
-done
+echo "[step6]:安装Golang......"
+cd /tmp
+wget https://dl.google.com/go/go1.17.1.linux-amd64.tar.gz
+tar -zxvf go1.17.1.linux-amd64.tar.gz
+mv go /usr/local/
+cd /usr/local/
+chmod -R 755 go
+echo "export GOROOT=/usr/local/go" >> /etc/profile
+echo "export PATH=$PATH:$GOROOT/bin" >> /etc/profile
+source /etc/profile
 
-if [ `echo $y_n | grep "^[y|Y]"` ]; then
-	echo "您想创建的用户名："
-	read username
-	until [ $username ]
-	do
-		read username
-	done
-	useradd -d /home/$username -m $username
-	echo "为新建用户赋予sudo权限"
-	chmod 644 /etc/sudoers
-	text="$username	ALL=(ALL)	ALL"
-	sed -i "100a ${text}" /etc/sudoers 
-	passwd $username
-	echo "$username用户创建成功！"
-fi 
-
+echo "[step7]:用户设置........"
+cd ~
+wget https://raw.githubusercontent.com/xhy-code/centos_shell_scripts/master/user_config.sh
+bash user_config.sh
 
 echo "系统初始化全部完成,enjoy yourself!!"
