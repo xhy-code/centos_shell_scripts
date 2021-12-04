7ee2f5f22b9852fde808c3fbe96e5db7a8a2313d
xhy-code <78998880+xhy-code@users.noreply.github.com>
2021-12-04 09:47:35 -0700

    Update aria2_config.sh

--- aria2_config.sh
+++ aria2_config.sh
@@ -2,6 +2,9 @@
 
 cd ~/.aria2c
 
+echo "Please input your domain name"
+read Domain
+
 sed -i 11c\drive-name=gdrive  script.conf
 sed -i 14c\drive-dir=/Download  script.conf
 sed -i 17c\upload-log=/root/.aria2c/upload.log  script.conf
@@ -9,7 +12,7 @@ sed -i 17c\upload-log=/root/.aria2c/upload.log  script.conf
 sed -i 301c\on-download-complete=/root/.aria2c/upload.sh aria2.conf
 sed -i 332c\rpc-secret=xiaohuyanghaoniubi84 aria2.conf
 sed -i 340c\rpc-secure=false aria2.conf
-sed -i 343c\rpc-certificate=/www/server/panel/vhost/ssl/aria2.xhyhub.xyz/fullchain.pem aria2.conf
-sed -i 346c\rpc-private-key=/www/server/panel/vhost/ssl/aria2.xhyhub.xyz/privkey.pem aria2.conf
+sed -i 343c\rpc-certificate=/www/server/panel/vhost/ssl/$Domain/fullchain.pem aria2.conf
+sed -i 346c\rpc-private-key=/www/server/panel/vhost/ssl/$Domain/privkey.pem aria2.conf
 
 service aria2 restart
